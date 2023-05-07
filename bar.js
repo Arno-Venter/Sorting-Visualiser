4eb3ca44580d76fc1b8fbb61c79267bc4c67c0be
Arno-Venter <arnoventer01@gmail.com>
2023-05-07 20:47:33 +0200

    added bubble sort

--- bar.js
+++ bar.js
@@ -1,13 +1,13 @@
 export default class Bar {
   constructor(value) {
     this.value = value;
+    this.barDiv = document.createElement("div");
   }
 
   buildBar(parent) {
-    const bar = document.createElement("div");
-    bar.style.width = "10px";
-    bar.style.height = this.value * 100 + "%";
-    bar.style.backgroundColor = "red";
-    parent.appendChild(bar);
+    this.barDiv.style.width = "10px";
+    this.barDiv.style.height = this.value * 100 + "%";
+    this.barDiv.style.backgroundColor = "red";
+    parent.appendChild(this.barDiv);
   }
 }
--- script.js
+++ script.js
@@ -1,18 +1,60 @@
 import Bar from "./bar.js";
 
-const SIZE = 100;
+const SIZE = 1000;
+const DELAY = 0.01;
 
-window.onload = setup;
+//window.onload = setup;
 
 let container = document.getElementById("barContainer");
+let startBtn = document.getElementById("sortBtn");
 let bars = [];
+let data = [];
+
+async function sleep(millis) {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(), millis);
+  });
+}
 
 function setup() {
-  let data = [];
-  for (let i = 0; i < 100; i++) {
-    data[i] = Math.random();
+  for (let i = 0; i < SIZE; i++) data[i] = Math.random();
+  buildBars();
+}
+
+function buildBars() {
+  if (bars.length !== 0) {
+    for (let i = 0; i < SIZE; i++) {
+      bars[i].barDiv.remove();
+    }
+  }
+
+  bars = [];
+
+  for (let i = 0; i < SIZE; i++) {
     const bar = new Bar(data[i]);
     bar.buildBar(container);
     bars.push(bar);
   }
 }
+
+async function bubbleSort() {
+  for (let i = 0; i < data.length; i++)
+    for (let j = 0; j < data.length - 1; j++) {
+      if (data[j] > data[j + 1]) {
+        let temp = data[j];
+        data[j] = data[j + 1];
+        data[j + 1] = temp;
+
+        await sleep(DELAY).then(() => {
+          buildBars();
+        });
+      }
+    }
+}
+
+setup();
+
+startBtn.addEventListener("click", () => {
+  console.log("OKAY");
+  bubbleSort();
+});
